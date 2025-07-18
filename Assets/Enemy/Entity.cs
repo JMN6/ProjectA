aa3f834131eee3506f62075e500726b4449d37ea
csy-59 <csy010509@naver.com>
2025-07-18 20:38:07 +0900

    [Feat] 플레이어, 에너미 상위 부모 스크립트 완성

---
+++ Assets/Enemy/Entity.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+    [SerializeField] protected int maxHealth; // 최대 생명력
+    [SerializeField] protected int currentHealth; // 현재 생명력
+    [SerializeField] protected float speed; // 이동속도
+    [SerializeField] protected int damage; // 공격력
+
+    protected abstract void GetDamaged(int damage);
+}
