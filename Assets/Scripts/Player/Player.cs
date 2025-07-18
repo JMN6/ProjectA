180aa7957514f055f1e1c44609e81812a01afa68
homebd <118789343+homebd@users.noreply.github.com>
2025-07-19 08:53:27 +0900

    [Feat] Make Player Anim

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,8 @@ public class Player : Entity, IDamagalbe
     [SerializeField] private float jumpPower;
 
     private Rigidbody2D rigid;
+    private SpriteRenderer sprite;
+    private Animator animator;
     private float moveDirect;
     private int curJumpCount;
     [SerializeField] private int maxJumpCount = 1;
@@ -13,11 +15,16 @@ public class Player : Entity, IDamagalbe
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     public void OnMove(InputValue input)
     {
         moveDirect = input.Get<float>();
+
+        if (moveDirect > 0 && sprite.flipX) sprite.flipX = false;
+        else if (moveDirect < 0 && !sprite.flipX) sprite.flipX = true;
     }
 
     public void OnJump()
@@ -26,21 +33,30 @@ public class Player : Entity, IDamagalbe
         {
             ++curJumpCount;
 
+            animator.SetTrigger("Jump");
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
 
     private void Update()
     {
+        Debug.Log(curJumpCount);
         rigid.velocity = new Vector2(moveDirect * speed, rigid.velocity.y);
-    }
+        animator.SetBool("IsWalking", moveDirect != 0);
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        // ¶¥ÀÌ¸é
-        if (true)
+        if (rigid.velocity.y < 0.01f)
         {
-            curJumpCount = 0;
+            RaycastHit2D hit = Physics2D.Raycast(rigid.position, Vector2.down, 0.1f, 1 << LayerMask.NameToLayer("Ground")); // ¶¥ ·¹ÀÌ¾î¸¸
+
+            if (hit)
+            {
+                curJumpCount = 0;
+                animator.SetBool("IsFalling", false);
+            }
+            else
+            {
+                animator.SetBool("IsFalling", true);
+            }
         }
     }
 
