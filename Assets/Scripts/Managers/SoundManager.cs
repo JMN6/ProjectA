6c2086e319522afd9facbe39008aca8cb1ea5dc5
homebd <118789343+homebd@users.noreply.github.com>
2025-07-19 10:27:06 +0900

    [Feat] Apply Some Sounds

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    private const int SFXCHANNEL = 8;
+
+    private AudioSource _bgmSource;
+    private List<AudioSource> _sfxAudio;
+
+    [SerializeField] private AudioClip _bgm;
+
+    private void Awake()
+    {
+        _sfxAudio = new();
+
+        CreateAudioSources(SFXCHANNEL);
+    }
+
+    private void Start()
+    {
+        PlayBGM();
+    }
+
+    private void CreateAudioSources(int size)
+    {
+        var rootObj = new GameObject("Audio").transform;
+        rootObj.SetParent(transform);
+
+        _bgmSource = new GameObject($"Music").AddComponent<AudioSource>();
+        _bgmSource.transform.SetParent(rootObj);
+        _bgmSource.volume = 0.2f;
+        _bgmSource.loop = true;
+        _bgmSource.playOnAwake = false;
+
+        for (int i = 0; i < size; i++)
+        {
+            var obj = new GameObject($"Audio{i}").AddComponent<AudioSource>();
+            obj.transform.SetParent(rootObj);
+            obj.playOnAwake = false;
+            _sfxAudio.Add(obj);
+        }
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        foreach (var audio in _sfxAudio)
+        {
+            if (!audio.isPlaying)
+            {
+                audio.PlayOneShot(clip);
+                return;
+            }
+        }
+    }
+
+    public void PlayBGM()
+    {
+        _bgmSource.clip = _bgm;
+        _bgmSource.Play();
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        foreach (var audio in _sfxAudio)
+        {
+            if (!audio.isPlaying)
+            {
+                audio.clip = clip;
+                audio.Play();
+                return;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,23 +8,39 @@ public class Player : Entity, IDamagalbe
     private Rigidbody2D rigid;
     private SpriteRenderer sprite;
     private Animator animator;
-    private float moveDirect;
+    private float movementDirection;
     private int curJumpCount;
     [SerializeField] private int maxJumpCount = 1;
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip jumpSFX;
+    [SerializeField] private AudioClip deathSFX;
+
+    private AudioSource audioSource;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void OnMove(InputValue input)
     {
-        moveDirect = input.Get<float>();
+        movementDirection = input.Get<float>();
 
-        if (moveDirect > 0 && sprite.flipX) sprite.flipX = false;
-        else if (moveDirect < 0 && !sprite.flipX) sprite.flipX = true;
+        if(movementDirection == 0)
+        {
+            audioSource.Stop();
+        }
+        else
+        {
+            audioSource.Play();
+
+            if (movementDirection > 0 && sprite.flipX) sprite.flipX = false;
+            else if (movementDirection < 0 && !sprite.flipX) sprite.flipX = true;
+        }
     }
 
     public void OnJump()
@@ -34,6 +50,7 @@ public class Player : Entity, IDamagalbe
             ++curJumpCount;
 
             animator.SetTrigger("Jump");
+            SoundManager.Instance.PlayOneShot(jumpSFX);
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
@@ -41,8 +58,8 @@ public class Player : Entity, IDamagalbe
     private void Update()
     {
         Debug.Log(curJumpCount);
-        rigid.velocity = new Vector2(moveDirect * speed, rigid.velocity.y);
-        animator.SetBool("IsWalking", moveDirect != 0);
+        rigid.velocity = new Vector2(movementDirection * speed, rigid.velocity.y);
+        animator.SetBool("IsWalking", movementDirection != 0);
 
         if (rigid.velocity.y < 0.01f)
         {
@@ -62,5 +79,6 @@ public class Player : Entity, IDamagalbe
 
     public void GetDamaged(int damage)
     {
+        SoundManager.Instance.PlayOneShot(deathSFX);
     }
 }
