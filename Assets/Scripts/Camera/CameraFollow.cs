3403896e9f59acbb1b7bc3f5044b1fc928f75680
csy-59 <csy010509@naver.com>
2025-07-20 00:18:30 +0900

    [Feat] 카메라 범위 제한 코드 추가

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -7,9 +8,14 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Vector2 padding;
     private Rigidbody2D rigid;
 
+    private Camera cam;
+    [SerializeField] private Transform bottom;
+    [SerializeField] private Transform top;
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        cam = GetComponent<Camera>();
     }
 
     public void SetFollow(Transform transform)
@@ -19,34 +25,35 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (follow != null)
-        {
-            var dx = transform.position.x - follow.position.x; // 양수면 카메라가 오른쪽
-            var dy = transform.position.y - follow.position.y; // 양수면 카메라가 위쪽
+        if (follow == null) return;
 
-            if(dx < -padding.x)
-            {
-                transform.Translate(-padding.x - dx, 0, 0);
-            }
-            else if(dx > padding.x)
-            {
-                transform.Translate(padding.x - dx, 0, 0);
-            }
+        float camHalfHeight = cam.orthographicSize;
+        float camHalfWidth = camHalfHeight * cam.aspect;
 
-            if (dy < -padding.y)
-            {
-                transform.Translate(0, -padding.y - dy, 0);
-            }
-            else if (dy > padding.y)
-            {
-                transform.Translate(0, padding.y - dy, 0);
-            }
+        Vector3 camPos = transform.position;
+        Vector2 targetPos = follow.position;
 
-            transform.position += offset;
-        }
-        else
-        {
-            Debug.LogError("Follow is null!!!!!!!!!!!!!!!!!!!");
-        }
+        // Dead zone 범위 계산
+        float leftBound = camPos.x - padding.x;
+        float rightBound = camPos.x + padding.x;
+        float bottomBound = camPos.y - padding.y;
+        float topBound = camPos.y + padding.y;
+
+        float newCamX = camPos.x;
+        float newCamY = camPos.y;
+
+        // 플레이어가 데드존 바깥에 있는 경우만 카메라 이동
+        if (targetPos.x < leftBound) newCamX = targetPos.x + padding.x;
+        else if (targetPos.x > rightBound) newCamX = targetPos.x - padding.x;
+
+        if (targetPos.y < bottomBound) newCamY = targetPos.y + padding.y;
+        else if (targetPos.y > topBound) newCamY = targetPos.y - padding.y;
+
+        // 맵 경계 제한 (카메라 크기 포함)
+        float clampedX = Mathf.Clamp(newCamX, bottom.position.x + camHalfWidth, top.position.x - camHalfWidth);
+        float clampedY = Mathf.Clamp(newCamY, bottom.position.y + camHalfHeight, top.position.y - camHalfHeight);
+
+        // 위치 반영
+        transform.position = new Vector3(clampedX, clampedY, camPos.z);
     }
 }
